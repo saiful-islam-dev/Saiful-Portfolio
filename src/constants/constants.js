8cf90c0d081283a150a62e4cf06d231117168511
Saiful264 <saifulislamx264@gmail.com>
2025-01-06 00:07:10 +0600

    Start working in contact me section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -26,33 +26,6 @@ export const projects = [
     visit: 'https://forum.xda-developers.com/t/tweak-mod-arm-x86-project-dark-booster-abs-tweaks-v5-0-2-3-6-0-23-01-2016.3120404/',
     id: 2,
   },
-  // {
-  //   title: 'ElectraBlue Kernel',
-  //   description: "ElectraBlue is a flash & forget custom kernel for supported Android devices. It was developed with the aim to provide a stable, fluid, & battery-efficient experience with customizations.",
-  //   image: '/images/eb.jpg',
-  //   tags: ['Linux', 'C', 'Makefile', 'Bash'],
-  //   source: 'https://github.com/lordarcadius/electrablue_mido',
-  //   visit: 'https://forum.xda-developers.com/t/kernel-mido-oreo-pie-electrablue-kernel-21-0-july-06-redmi-note-4.3655651/',
-  //   id: 3,
-  // },
-  // {
-  //   title: 'Lenovo SNAPit',
-  //   description: "Lenovo SNAPit Camera was one of the finest OEM camera apps back in those days. It had tonnes of unique and amazing features. I ported it to work on almost all devices at that time.",
-  //   image: '/images/snapit.jpg',
-  //   tags: ['Java', 'Libs', 'Smali'],
-  //   source: '#',
-  //   visit: 'https://forum.xda-developers.com/t/app-port-6-0-lenovo-snapit-camera-5-8-53-for-all-devices.3608065/',
-  //   id: 4,
-  // },
-  // {
-  //   title: 'CyanogenOS Apps',
-  //   description: "CyanogenOS 12.1 had a set of exclusive apps like a new theme engine and a new Truecaller integrated dialer. I ported it to work with CyanogenMod 13 and CM 13 based ROMs.",
-  //   image: '/images/capps.jpg',
-  //   tags: ['Java', 'Libs'],
-  //   source: '#',
-  //   visit: 'https://forum.xda-developers.com/t/c-apps-v2-unofficial-6-0-x-cyanogen-os-capps-v2-for-cm13-and-cm13-based-roms.3254865/',
-  //   id: 5,
-  // },
   {
     title: 'Titans Arena',
     description: "A gaming platform featuring official game information, pre-registration, tournament management, and a gaming blog. Built using React, Tailwind CSS, and Redux RTK Query for the frontend and Firebase with MongoDB and Express for secure, scalable backend functionality.",
@@ -90,3 +63,35 @@ export const TimeLineData = [
   { year: 2023, text: 'Learned the MERN Stack and took a course from Programming Hero. Competed in a team competition, finishing in the top 5.' },
   { year: 2024, text: 'Advanced my skills with new technologies and enrolled in another course from Programming Hero.' },
 ];
+
+
+
+
+
+  // {
+  //   title: 'ElectraBlue Kernel',
+  //   description: "ElectraBlue is a flash & forget custom kernel for supported Android devices. It was developed with the aim to provide a stable, fluid, & battery-efficient experience with customizations.",
+  //   image: '/images/eb.jpg',
+  //   tags: ['Linux', 'C', 'Makefile', 'Bash'],
+  //   source: 'https://github.com/lordarcadius/electrablue_mido',
+  //   visit: 'https://forum.xda-developers.com/t/kernel-mido-oreo-pie-electrablue-kernel-21-0-july-06-redmi-note-4.3655651/',
+  //   id: 3,
+  // },
+  // {
+  //   title: 'Lenovo SNAPit',
+  //   description: "Lenovo SNAPit Camera was one of the finest OEM camera apps back in those days. It had tonnes of unique and amazing features. I ported it to work on almost all devices at that time.",
+  //   image: '/images/snapit.jpg',
+  //   tags: ['Java', 'Libs', 'Smali'],
+  //   source: '#',
+  //   visit: 'https://forum.xda-developers.com/t/app-port-6-0-lenovo-snapit-camera-5-8-53-for-all-devices.3608065/',
+  //   id: 4,
+  // },
+  // {
+  //   title: 'CyanogenOS Apps',
+  //   description: "CyanogenOS 12.1 had a set of exclusive apps like a new theme engine and a new Truecaller integrated dialer. I ported it to work with CyanogenMod 13 and CM 13 based ROMs.",
+  //   image: '/images/capps.jpg',
+  //   tags: ['Java', 'Libs'],
+  //   source: '#',
+  //   visit: 'https://forum.xda-developers.com/t/c-apps-v2-unofficial-6-0-x-cyanogen-os-capps-v2-for-cm13-and-cm13-based-roms.3254865/',
+  //   id: 5,
+  // },
